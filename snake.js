a97f5e2b2afd6a52537eccefd48257b4577ba356
mohan <mohankukreja1@gmail.com>
2018-03-16 17:31:42 +0530

    audio added
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- snake.js
+++ snake.js
@@ -13,6 +13,20 @@ ground.src="img/ground.png";
 const food=new Image();
 food.src="img/food.png";
 
+const dead=new Audio();
+const up=new Audio();
+const down=new Audio();
+const left=new Audio();
+const right=new Audio();
+const eat=new Audio();
+
+
+dead.src="audio/dead.mp3";
+up.src="audio/up.mp3";
+down.src="audio/down.mp3";
+left.src="audio/left.mp3";
+right.src="audio/right.mp3";
+eat.src="audio/eat.mp3";
 let snake=[];
 
 snake[0]={
@@ -27,21 +41,27 @@ let foodPlace={
 
 let score=0;
 var d;
+
+
 document.addEventListener("keydown",direction);
 
 function direction(event) {
     let key=event.keyCode
     if(key == 37 && d!="RIGHT"){
         d= "LEFT";
+        left.play();
     }
     else if(key == 38 && d!="DOWN"){
         d= "UP";
+        up.play();
     }
     else if(key == 39 && d!="LEFT"){
         d= "RIGHT";
+        right.play()
     }
     else if(key == 40 && d!= "UP"){
         d= "DOWN";
+        down.play();
     }
 
 }
@@ -59,14 +79,32 @@ function draw() {
 
     ctx.drawImage(food,foodPlace.x,foodPlace.y);
 
+    console.log(eat)
+
     let snakeX=snake[0].x;
     let snakeY=snake[0].y;
-if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box){
+
+    let newhead={
+        x: snakeX,
+        y: snakeY
+    }
+
+    function collision(head,array) {
+        for(let i=0;i<array.length;i++){
+            if(head.x == array[i].x && head.x == array[i].y){
+                return true;
+            }
+        }
+         return false;
+    }
+if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newhead,snake)){
     clearInterval(game)
+    dead.play();
 }
 
     if(snakeX == foodPlace.x && snakeY == foodPlace.y){
         score++;
+        eat.play();
         foodPlace={
             x:Math.floor(Math.random()*17+1) * box,
             y:Math.floor(Math.random()*15+3) * box
@@ -85,10 +123,7 @@ if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box){
     if(d == "UP") snakeY -= box;
     if(d == "DOWN") snakeY += box;
 
-    let newhead={
-        x: snakeX,
-        y: snakeY
-    }
+
 
     snake.unshift(newhead);
 
